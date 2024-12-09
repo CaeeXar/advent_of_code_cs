17ead3f32c55a77b755f2ae407a7481d8a78f9fd
Samuel Erdö <samuelerdo@gmx.at>
2024-12-09 01:09:50 +0100

    2024: day 6 part 1

---
+++ AdventOfCode/2024/day06/Day06.cs
@@ -0,0 +1,114 @@
+﻿namespace AOC2024.Day06
+{
+    internal class Day06
+    {
+        string path = @"2024\day06\Input.txt";
+
+        private class Point
+        {
+            public Point (int x, int y)
+            {
+                this.X = x;
+                this.Y = y;
+            }
+
+            public int X { get; set; }
+            public int Y { get; set; }
+        }
+
+        private enum Map
+        {
+            PLAYER = '^',
+            OBSTACLE = '#',
+            FREE = '.',
+            MARKED = 'X',
+        }
+
+        public string ReadFile()
+        {
+            try
+            {
+                return File.ReadAllText(path);
+            }
+            catch (Exception e)
+            {
+                System.Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+
+        public int Part1() 
+        {
+            char[][] map = ReadFile().Split(new string[] { "\r\n", "\n", "\r" }, StringSplitOptions.None)
+                                     .Select(d => d.Trim().ToCharArray())
+                                     .ToArray();
+            Point current = FindStartPosition(map);
+            Point direction = new Point(0, -1);
+            int steps = 1;
+            while (true)
+            {
+                map[current.Y][current.X] = (char)Map.MARKED;
+                Point next = new Point(current.X + direction.X, current.Y + direction.Y);
+                if (next.X < 0 || next.X >= map[0].Length ||
+                    next.Y < 0 || next.Y >= map.Length)
+                {
+                    return steps;
+                }
+                else if (map[next.Y][next.X] == (char)Map.OBSTACLE)
+                {
+                    ChangeDirection(ref direction);
+                    continue;
+                }
+                else if (map[next.Y][next.X] != (char)Map.MARKED)
+                {
+                    steps++;
+                }
+
+                current.X = next.X;
+                current.Y = next.Y;
+            }
+        }
+
+        public int Part2()
+        {
+            return 0;
+        }
+
+        private void ChangeDirection(ref Point direction)
+        {
+            if (direction.Y == -1)
+            {
+                direction.X = 1;
+                direction.Y = 0;
+            }
+            else if (direction.X == 1)
+            {
+                direction.X = 0;
+                direction.Y = 1;
+            }
+            else if (direction.Y == 1)
+            {
+                direction.X = -1;
+                direction.Y = 0;
+            }
+            else if (direction.X == -1)
+            {
+                direction.X = 0;
+                direction.Y = -1;
+            }
+        }
+
+        private Point FindStartPosition(char[][] map)
+        {
+            for (int row = 0; row < map.Length; row++)
+            {
+                for (int col = 0; col < map[row].Length; col++)
+                {
+                    if (map[row][col] == (char)Map.PLAYER) return new Point(col, row);
+                }
+            }
+
+            return null;
+        }
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,32 +1,4 @@
-﻿// 2017
-using AOC2017.Day01;
-using AOC2017.Day02;
-using AOC2017.Day03;
-using AOC2017.Day04;
-using AOC2017.Day05;
-using AOC2017.Day06;
-using AOC2017.Day07;
-using AOC2017.Day08;
-using AOC2017.Day09;
-using AOC2017.Day10;
-using AOC2017.Day11;
-using AOC2017.Day12;
-using AOC2017.Day13;
-using AOC2017.Day14;
-using AOC2017.Day15;
-using AOC2017.Day16;
-using AOC2017.Day17;
-using AOC2017.Day18;
-using AOC2017.Day19;
-
-// 2024
-using AOC2024.Day01;
-using AOC2024.Day02;
-using AOC2024.Day03;
-using AOC2024.Day04;
-using AOC2024.Day05;
-
-namespace AOC
+﻿namespace AOC
 {
     internal class Program
     {
@@ -38,7 +10,7 @@ namespace AOC
             //System.Console.WriteLine($"\t- Part 2: {day.Part2()}");
 
             // 2024
-            AOC2024.Day05.Day05 day = new AOC2024.Day05.Day05();
+            AOC2024.Day06.Day06 day = new AOC2024.Day06.Day06();
             System.Console.WriteLine($"\t- Part 1: {day.Part1()}\n");
             System.Console.WriteLine($"\t- Part 2: {day.Part2()}");
         }
