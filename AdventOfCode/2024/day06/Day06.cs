242ba8e9aebc518369f636c84957fcdb7c6cd6a1
Samuel Erdö <samuelerdo@gmx.at>
2024-12-09 17:53:42 +0100

    2024: day 6 part 2

--- AdventOfCode/2024/day06/Day06.cs
+++ AdventOfCode/2024/day06/Day06.cs
@@ -14,14 +14,23 @@
 
             public int X { get; set; }
             public int Y { get; set; }
-        }
 
-        private enum Map
-        {
-            PLAYER = '^',
-            OBSTACLE = '#',
-            FREE = '.',
-            MARKED = 'X',
+            public override string ToString()
+            {
+                return $"({this.X}, {this.Y})";
+            }
+
+            public override int GetHashCode()
+            {
+                return this.X + this.Y;
+            }
+        
+            public override bool Equals(object? obj)
+            {
+                if (obj == null) return false;
+                Point other = (Point)obj;
+                return (other.X == this.X && other.Y == this.Y);
+            }
         }
 
         public string ReadFile()
@@ -42,24 +51,59 @@
             char[][] map = ReadFile().Split(new string[] { "\r\n", "\n", "\r" }, StringSplitOptions.None)
                                      .Select(d => d.Trim().ToCharArray())
                                      .ToArray();
+            return CountGuardSteps(map);
+        }
+
+        public int Part2()
+        {
+            char[][] origMap = ReadFile().Split(new string[] { "\r\n", "\n", "\r" }, StringSplitOptions.None)
+                                     .Select(d => d.Trim().ToCharArray())
+                                     .ToArray();
+            char[][] map = origMap.Select(d => d.Select(c => c).ToArray()).ToArray();
+
+            int loops = 0;
+            for (int row = 0; row < map.Length; row++)
+            {
+                for (int col = 0; col < map[row].Length; col++)
+                {
+                    if (map[row][col] != '#' && map[row][col] != '^')
+                    {
+                        map[row][col] = '#';
+                        if (CountGuardSteps(map) == -1) loops++;
+                        map = origMap.Select(d => d.Select(c => c).ToArray()).ToArray();
+                    }
+                }
+            }
+
+            return loops;
+        }
+
+        private int CountGuardSteps(char[][] map)
+        {
             Point current = FindStartPosition(map);
             Point direction = new Point(0, -1);
+            Dictionary<Point, int> edges = new Dictionary<Point, int>();
             int steps = 1;
             while (true)
             {
-                map[current.Y][current.X] = (char)Map.MARKED;
+                if (edges.ContainsKey(current) && edges[current] >= 3) return -1;
+                
+                map[current.Y][current.X] = 'X';
                 Point next = new Point(current.X + direction.X, current.Y + direction.Y);
+
                 if (next.X < 0 || next.X >= map[0].Length ||
                     next.Y < 0 || next.Y >= map.Length)
                 {
                     return steps;
                 }
-                else if (map[next.Y][next.X] == (char)Map.OBSTACLE)
+                else if (map[next.Y][next.X] == '#')
                 {
+                    if (edges.ContainsKey(current)) edges[current]++;
+                    else edges.Add(new Point(current.X, current.Y), 1);
                     ChangeDirection(ref direction);
                     continue;
                 }
-                else if (map[next.Y][next.X] != (char)Map.MARKED)
+                else if (map[next.Y][next.X] != 'X')
                 {
                     steps++;
                 }
@@ -67,11 +111,6 @@
                 current.X = next.X;
                 current.Y = next.Y;
             }
-        }
-
-        public int Part2()
-        {
-            return 0;
         }
 
         private void ChangeDirection(ref Point direction)
@@ -104,7 +143,7 @@
             {
                 for (int col = 0; col < map[row].Length; col++)
                 {
-                    if (map[row][col] == (char)Map.PLAYER) return new Point(col, row);
+                    if (map[row][col] == '^') return new Point(col, row);
                 }
             }
 
